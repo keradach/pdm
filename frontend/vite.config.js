b9d60627468d903a7c9151f1356ba14d1b877672
กีรเดช โรจน์กิจอนันต์ <keradach.r@gmail.com>
2026-08-13 09:30:54 +0700

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ["pdmrc.doae.go.th"],
   },
-})
+});
